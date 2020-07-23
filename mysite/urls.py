fafe525099318abeb5c25acc1ca1d91ca2c8f92f
musleh0001 <khan.muslehuddin0001@gmail.com>
2020-07-23 12:16:55 +0600

    change urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

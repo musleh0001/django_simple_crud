804c09086bb6b34810f8efb7a6af63817636c002
musleh0001 <khan.muslehuddin0001@gmail.com>
2020-07-23 12:06:30 +0600

    fixed wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
